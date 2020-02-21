0334dcf8ceddd989803fefe7887000802ef09633
Tony Chou <tcheon8788@gmail.com>
2020-02-21 16:34:35 +0800

    Updates

---
+++ precache-manifest.da76b64c670476f999242d025f35a1cf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b077dc41298871acf494fda475498ca",
+    "url": "/handoff-visualizer/index.html"
+  },
+  {
+    "revision": "473efa15d2e18f94d424",
+    "url": "/handoff-visualizer/static/css/main.459cb5fe.chunk.css"
+  },
+  {
+    "revision": "8d46676ec12e9a8cc366",
+    "url": "/handoff-visualizer/static/js/2.8e5453d4.chunk.js"
+  },
+  {
+    "revision": "cb6ca074a628e298c4ff3d47d133bbee",
+    "url": "/handoff-visualizer/static/js/2.8e5453d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "473efa15d2e18f94d424",
+    "url": "/handoff-visualizer/static/js/main.d981b0e3.chunk.js"
+  },
+  {
+    "revision": "8f5e85624f0b309c8832",
+    "url": "/handoff-visualizer/static/js/runtime-main.384b9427.js"
+  },
+  {
+    "revision": "c20a29851c583dcdcf5c65cbc31e7450",
+    "url": "/handoff-visualizer/static/media/arrow0.c20a2985.svg"
+  },
+  {
+    "revision": "2cc29864d739b4750016db082048d416",
+    "url": "/handoff-visualizer/static/media/arrow1.2cc29864.svg"
+  },
+  {
+    "revision": "5c36b00b0ec2f10e85f8931ec86a9fb5",
+    "url": "/handoff-visualizer/static/media/arrow2.5c36b00b.svg"
+  },
+  {
+    "revision": "201a977cb75418a541a5d09e6805b374",
+    "url": "/handoff-visualizer/static/media/arrow3.201a977c.svg"
+  },
+  {
+    "revision": "eeebed5288fe54978844a6a564440cae",
+    "url": "/handoff-visualizer/static/media/bs.eeebed52.svg"
+  },
+  {
+    "revision": "33aa5d42988f49428f99617d9f84053b",
+    "url": "/handoff-visualizer/static/media/handoff3.33aa5d42.svg"
+  }
+]);
